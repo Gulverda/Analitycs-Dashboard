ffdfbd2c9d597547bb71e939a601cb94074dfcb7
luka gulverdashvili <lukagulverdashvili49@gmail.com>
2025-07-08 12:28:57 +0400

    Add: Folder/File Structure of Server

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
@@ -14,7 +15,18 @@ app.get('/', (req, res) => {
   res.send('Backend Server is running!');
 });
 
-app.listen(PORT, () => {
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI as string)
+    console.log('MongoDB connected successfully');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+}
+
+app.listen(PORT, async () => {
+  await connectDB();
   console.log(`Server is running on port ${PORT}`);
   console.log(`Access at http://localhost:${PORT}`);
 });
